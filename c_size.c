933ac41fd734ea58394980d19bd724794d2155c4
TungNT2000 <nguyenthanhtung082k@gmail.com>
2024-07-04 20:31:41 +0700

    change c_size

--- c_size.c
+++ c_size.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-int f1() {
-  printf("Geeks");
-  return 1;
-}
-int f2() {
-  printf("Quiz");
-  return 1;
-}
-
+#define STRING "Hello_World"
 int main() {
-  int p = f1() + f2();
+  printf(STRING);
   return 0;
 }
